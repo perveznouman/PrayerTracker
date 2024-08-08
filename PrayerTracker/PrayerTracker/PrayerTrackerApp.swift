8c47d7f7f440a34726be55008fd3c4385fe7fc41
Nouman Pervez <perveznouman@gmail.com>
2024-08-09 00:30:19 +0530

    Removing built in coreData code

--- PrayerTracker/PrayerTracker/PrayerTrackerApp.swift
+++ PrayerTracker/PrayerTracker/PrayerTrackerApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct PrayerTrackerApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             PTRootView()
-//            ContentView().environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
